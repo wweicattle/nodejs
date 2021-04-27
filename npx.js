14d8530a0b043b44f7264135dd3ee740ea03694b
xiaowei <996196542@qq.com>
2021-04-27 22:47:51 +0800

    add_npx

---
+++ npx.js
@@ -0,0 +1,6 @@
+//他是一个什么呢，
+// 他是一个快捷的访问本地版本
+// 当你全局安装webpack，你项目又安装了webpack那么你webpack -v 是会执行全局的webpack的版本。
+// 除非你使用在该项目目录下使用，npx webpack -v 来进行运行
+//在脚本script中你也可以使用npm run webpack -v ，因为你package.json中呢会优先去寻找node_Modules中的文件
+// 
